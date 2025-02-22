ae6c1db7501a6ac0c685a826b4890bc12b8515b3
tanvirabsar <tjoy4799@gmail.com>
2025-02-22 17:50:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    // other configurations...
+    daisyui: {
+      themes: false, // This will disable all themes except the default light theme
+      // or you can specify only the light theme
+      // themes: ["light"],
+    },
+  };
+  module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-start': 'rgb(164, 188, 70)',
+          'custom-end': 'rgb(133, 160, 25)',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  };
+  
